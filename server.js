8ee4f17f8a4d833b51ce5d4046f1cf5478d1ba7c
Victor Gonzalez <vitorpcg@gmail.com>
2023-09-14 18:29:08 +0100

    teste
    http

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import fastify from 'fastify'
+import http from 'node:http'
 
 // import { DatabaseMemory } from './database-memory.js'
 import { DatabasePostgres } from './database-postgres.js'
 
-const server = fastify()
+const server = http.createServer()
 
 // // const database = new DatabaseMemory()
 const database = new DatabasePostgres()
