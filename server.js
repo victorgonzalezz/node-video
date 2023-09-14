fd6e1d03db05ea45c779906fd48b51006e1fab4c
Victor Gonzalez <vitorpcg@gmail.com>
2023-09-14 17:07:52 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+import { fastify } from 'fastify'
+
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+// // const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+
+server.post('/videos', async (request, respond) => {
+    const {title, description, duration} = request.body
+
+    
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration,
+    })
+    
+
+    return  respond.status(201).send()   
+    
+})
+
+server.get('/videos',  async (request) => {
+    const search = request.query.search
+
+    const videos =  await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id',  async (request, respond) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+    
+    await database.list(videoId, {
+        title: title,
+        description: description,
+        duration: duration
+    })
+
+    return respond.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, respond) => {
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+
+    return respond.status(204).send
+})
+
+server.listen({
+    // eslint-disable-next-line no-undef
+    port: process.env.PORT ?? 3333,
+})
