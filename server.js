2e95ad7fd086ae1ba5fb0dba4affdcab9f3d5e21
Victor Gonzalez <vitorpcg@gmail.com>
2023-09-14 18:13:45 +0100

    node
    node

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { fastify } from 'fastify'
+import fastify from 'fastify'
 
 // import { DatabaseMemory } from './database-memory.js'
 import { DatabasePostgres } from './database-postgres.js'
